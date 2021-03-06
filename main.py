2e74e327d8b4fb9238626676b94bf51d5fcb90ed
nathann <natmo@hotmail.ch>
2021-03-06 19:37:01 +0100

    fixed stuff

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ import random
 import matplotlib.pyplot as plt
 from nxsim import BaseNetworkAgent
 
-MinimalLinkRule = 2
-MinimalLinkRule = MinimalLinkRule+2
-MaximalLinkRule = 5
-
 """
 Take the Graph an an argument and add a new Node Connected to a random ammount on Other Node
 """
@@ -22,25 +18,44 @@ Take the Graph an an argument and add a new Node Connected to a random ammount o
 class NodeBehaviour(BaseNetworkAgent):
     def __init__(self, environment=None, agent_id=0, state=()):  # Make sure to have these three keyword arguments
         super().__init__(environment=environment, agent_id=agent_id, state=state)
-        # Add your own attributes here
+        self.packetGenerationProbability = 0.05
+        self.maxNumberOfPacketPerSecondAllowed = 100
 
     def run(self):
-        # Add your behaviors here
+        """Simuate basic routing algorithm and generate random traffic toward a "random" node"""
+        while True:
+            if self.state['id'] == 1:
+                self.zombify()
+                yield self.env.timeout(1)
+            else:
+                yield self.env.event()
+
+    def generateTraffic(self):
+        normal_neighbors = self.get_neighboring_agents(state_id=0)
+        for neighbor in normal_neighbors:
+            if random.random() < self.bite_prob:
+                neighbor.state['id'] = 1  # zombie
+                print(self.env.now, self.id, neighbor.id, sep='\t')
+                break
 
 class NetworkTopology:
     def __init__(self):  # Make sure to have these three keyword arguments
         self.G = nx.Graph()
 
     def addNodePlusRandomLink(self):
+        minimalLinkRule = 2
+        minimalLinkRule = minimalLinkRule + 2
+        maximalLinkRule = 5
         n = len(self.G.nodes())
         nodeList = list(self.G.nodes())
         self.G.add_node(str(n))
         n = len(self.G.nodes())
         if n>1: # we need at least two node to create a link
-            if n > MinimalLinkRule+1: # we cant create more link than node so in the beginning the max n of link = n of node
+            if n > minimalLinkRule+1: # we cant create more link than node so
+                # in the beginning the max n of link = n of node
                 # once there are enough node, add a random number contained between two border
-                NumberOfLink = random.randint(MinimalLinkRule, MaximalLinkRule)
-                for i in range(0,NumberOfLink-2): # add the choosen number of link
+                numberOfLink = random.randint(minimalLinkRule, maximalLinkRule)
+                for i in range(0,numberOfLink-2): # add the choosen number of link
                     startNodeIndex = n-1
                     endNodeIndex = random.choice(nodeList) # connect to a random node accros the graph
                     nodeList.remove(endNodeIndex) # ensure we double link a node by removing it from the drafting pool
@@ -53,15 +68,18 @@ class NetworkTopology:
 
     def simulateLinkBetweeTwoNode(self,depart, arrival):
         hello = 1
+        print(hello)
+
 
 def main():
     network = NetworkTopology()
-    for i in range(0,80):
+    for i in range(0, 80):
         network.addNodePlusRandomLink()
     nx.draw(network.G, with_labels=True, font_weight="bold")
     plt.show()
 
     print("Done")
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     main()
